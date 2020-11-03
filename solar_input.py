74cf7867d30c8a7871d98f37e720fa9e0280cd27
korliakov <korliakov.dk@phystech.edu>
2020-11-03 13:07:33 +0300

    Начал менять, но нужно доделать

--- solar_input.py
+++ solar_input.py
@@ -19,12 +19,15 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":  
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
-            else:
-                print("Unknown space object")
+                
+            if object_type == "planet":  # FIXED
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
 
     return objects
 
@@ -43,6 +46,8 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+
+    
 
     pass  # FIXME: not done yet
 
